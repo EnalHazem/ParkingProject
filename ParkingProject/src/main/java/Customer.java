9304f25aa7e5c069033a951b7bfee2e313292081
EnalHazem <161242400+EnalHazem@users.noreply.github.com>
2025-07-04 19:00:45 +0300

    Update Customer.java

--- ParkingProject/src/main/java/Customer.java
+++ ParkingProject/src/main/java/Customer.java
@@ -1,13 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Seif Asharf
- */
-// Customer.java
 import java.util.Scanner;
 
 public class Customer extends User {
