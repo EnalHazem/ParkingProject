680ccc21244a6a716725ed39b35b6908f83dee8f
EnalHazem <161242400+EnalHazem@users.noreply.github.com>
2025-07-04 19:03:51 +0300

    Update Ticket.java

--- ParkingProject/src/main/java/Ticket.java
+++ ParkingProject/src/main/java/Ticket.java
@@ -1,12 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Seif Asharf
- */
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 
